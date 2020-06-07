9470138f779ee48dd69853442717fd13b3a5693f
Edd-wordd <edward.plasencio@gmail.com>
2020-06-07 03:38:49 -0700

    add pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,40 @@
 import React from "react";
 import "./style/master.scss";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Landing from "./components/pages/Landing";
+import CreateAnswer from "./components/pages/CreateAnswer";
+import CreateImagery from "./components/pages/CreateImagery";
+import ReviewImagery from "./components/pages/ReviewImagery";
+import ReviewAnswer from "./components/pages/ReviewAnswer";
+import ReviewEmpty from "./components/pages/ReviewEmpty";
+import AllCards from "./components/pages/AllCards";
+import Edit from "./components/pages/Edit";
+import NotFound from "./components/pages/NotFound";
 
 function App() {
-   return <h1 className="text-danger">Edward is here!</h1>;
+   return (
+      <Router>
+         <Switch>
+            <Route exact path="/" component={Landing}></Route>
+            <Route exact path="/create-answer" component={CreateAnswer}></Route>
+            <Route
+               exact
+               path="create-imagery/"
+               component={CreateImagery}
+            ></Route>
+            <Route
+               exact
+               path="/review-imagery"
+               component={ReviewImagery}
+            ></Route>
+            <Route exact path="/review-answer" component={ReviewAnswer}></Route>
+            <Route exact path="/review-empyty" component={ReviewEmpty}></Route>
+            <Route exact path="/all-cards" component={AllCards}></Route>
+            <Route exact path="/edit" component={Edit}></Route>
+            <Route component={NotFound}></Route>
+         </Switch>
+      </Router>
+   );
 }
 
 export default App;
