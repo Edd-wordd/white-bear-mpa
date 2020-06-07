906596157d90704554c8923302f6d183af2f56ad
Edd-wordd <edward.plasencio@gmail.com>
2020-06-07 02:47:37 -0700

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
+import "./style/master.scss";
 
 function App() {
-   return <h1>Edward is here!</h1>;
+   return <h1 className="text-danger">Edward is here!</h1>;
 }
 
 export default App;
