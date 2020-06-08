dc25375a400a5fe5510004ea8617adf2374331f2
Edd-wordd <edward.plasencio@gmail.com>
2020-06-08 12:49:01 -0700

    create template

--- src/components/pages/CreateImagery.js
+++ src/components/pages/CreateImagery.js
@@ -1,78 +1,67 @@
 import React from "react";
 import saveIcon from "../../icons/save.svg";
-import Header from "../UI/Header"
-import Navigation from "../UI/Navigaiton"
+import Header from "../UI/Header";
+import Navigation from "../UI/Navigaiton";
+import AppTemplate from "../UI/appTemplate";
 
 export default function CreateImagery() {
    return (
-      <div className="container">
-         <div className="row">
-            <div className="col-12 col-xl-6 offset-xl-3 col-lg-8 offset-lg-2 col-md-10 offset-md-1">
-            
-             <Header/>
-            <Navigation/>
+      <AppTemplate>
+         <Header />
+         <Navigation />
 
-               
-               <h4 className="my-4 text-center text-muted">
-                  Add memorable imagery
-               </h4>
-               {/* <!-- begin cards --> */}
-               <div className="mb-2">
-                  <div className="card bg-primary">
-                     <div className="card-body secondCard">
-                        <textarea
-                           id="imagery-create"
-                           className="w-100 d-sm-nope"
-                           rows="11"
-                           autoFocus={true}
-                        ></textarea>
-                        <textarea
-                           className="w-100 d-nope d-sm-block"
-                           rows="6"
-                        ></textarea>
-                     </div>
-                  </div>
-                  <div className="card bg-secondary">
-                     <div className="card-body secondCard">
-                        The European languages are members of the same family.
-                        Their separate existence is a myth. For science, music,
-                        sport, etc, Europe uses the same vocabulary. The
-                        languages only differ in their grammar, their
-                        pronunciation and their most common words.
-                     </div>
-                  </div>
+         <h4 className="my-4 text-center text-muted">Add memorable imagery</h4>
+         {/* <!-- begin cards --> */}
+         <div className="mb-2">
+            <div className="card bg-primary">
+               <div className="card-body secondCard">
+                  <textarea
+                     id="imagery-create"
+                     className="w-100 d-sm-nope"
+                     rows="11"
+                     autoFocus={true}
+                  ></textarea>
+                  <textarea
+                     className="w-100 d-nope d-sm-block"
+                     rows="6"
+                  ></textarea>
                </div>
-               {/* <!-- end cards --> */}
-               {/* <!-- begin button buttons --> */}
-               <p className="float-right mb-5" id="countMe2">
-                  0/240
-               </p>
-               <div className="clearfix"></div>
-               <a
-                  href="landing.html"
-                  className="btn btn-link"
-                  id="backToAnswer"
-               >
-                  Back to answer
-               </a>
-
-               <a
-                  id="save-imagery"
-                  href="landing.html"
-                  className="btn btn-primary btn-lg float-right disabled"
-               >
-                  <img
-                     className="mb-2"
-                     src={saveIcon}
-                     width="20px;"
-                     style={{ marginTop: "5px", marginRight: "8px" }}
-                     alt=""
-                  />
-                  Save
-               </a>
-               {/* <!-- end bottom buttons --> */}
+            </div>
+            <div className="card bg-secondary">
+               <div className="card-body secondCard">
+                  The European languages are members of the same family. Their
+                  separate existence is a myth. For science, music, sport, etc,
+                  Europe uses the same vocabulary. The languages only differ in
+                  their grammar, their pronunciation and their most common
+                  words.
+               </div>
             </div>
          </div>
-      </div>
+         {/* <!-- end cards --> */}
+         {/* <!-- begin button buttons --> */}
+         <p className="float-right mb-5" id="countMe2">
+            0/240
+         </p>
+         <div className="clearfix"></div>
+         <a href="landing.html" className="btn btn-link" id="backToAnswer">
+            Back to answer
+         </a>
+
+         <a
+            id="save-imagery"
+            href="landing.html"
+            className="btn btn-primary btn-lg float-right disabled"
+         >
+            <img
+               className="mb-2"
+               src={saveIcon}
+               width="20px;"
+               style={{ marginTop: "5px", marginRight: "8px" }}
+               alt=""
+            />
+            Save
+         </a>
+         {/* <!-- end bottom buttons --> */}
+      </AppTemplate>
    );
 }
--- src/components/pages/ReviewAnswer.js
+++ src/components/pages/ReviewAnswer.js
@@ -1,62 +1,56 @@
 import React from "react";
 import thumbsUpIcon from "../../icons/thumbs-up.svg";
-import Header from "../UI/Header"
-import Navigation from "../UI/Navigaiton"
-
+import Header from "../UI/Header";
+import Navigation from "../UI/Navigaiton";
+import AppTemplate from "../UI/appTemplate";
 
 export default function ReviewAnswer() {
    return (
-      <div className="container">
-         <div className="row">
-            <div className="col-12 col-xl-6 offset-xl-3 col-lg-8 offset-lg-2 col-md-10 offset-md-1">
-               <Header/>
-               <Navigation/>
-               <div className="mb5"></div>
+      <AppTemplate>
+         <Header />
+         <Navigation />
 
-               
-               <div className="mb-5">
-                  <div className="card bg-primary">
-                     <div className="card-body secondCard">
-                        The quick, brown fox jumps over a lazy dog. DJs flock by
-                        when MTV ax quiz prog. Junk MTV quiz graced by fox
-                        whelps. Bawds jog, flick quartz, vex nymphs. Waltz, bad
-                        nymph, for quick jigs vex! Fox nymphs grab quick-jived
-                        waltz. Brick quiz whangs jumpy veldt fox.
-                     </div>
-                  </div>
-                  <div className="card bg-secondary">
-                     <div className="card-body secondCard">
-                        The quick, brown fox jumps over a lazy dog. DJs flock by
-                        when MTV ax quiz prog. Junk MTV quiz graced by fox
-                        whelps. Bawds jog, flick quartz, vex nymphs. Waltz, bad
-                        nymph, for quick jigs vex! Fox nymphs grab quick-jived
-                        waltz. Brick quiz whangs jumpy veldt fox.
-                     </div>
-                  </div>
+         <div className="mb-5">
+            <div className="card bg-primary">
+               <div className="card-body secondCard">
+                  The quick, brown fox jumps over a lazy dog. DJs flock by when
+                  MTV ax quiz prog. Junk MTV quiz graced by fox whelps. Bawds
+                  jog, flick quartz, vex nymphs. Waltz, bad nymph, for quick
+                  jigs vex! Fox nymphs grab quick-jived waltz. Brick quiz whangs
+                  jumpy veldt fox.
                </div>
-               <button href="edit.html" className="btn btn-link">
-                  Edit card
-               </button>
-               <div className="float-right">
-                  <button
-                     href="review-done.html"
-                     className="btn btn-outline-primary mr-1"
-                  >
-                     Needs Work
-                  </button>
-                  <button href="review-done.html" className="btn btn-primary">
-                     <img
-                        className="mb-2"
-                        src={thumbsUpIcon}
-                        width="20px;"
-                        style={{ marginRight: "7px" }}
-                        alt=""
-                     />
-                     Got it
-                  </button>
+            </div>
+            <div className="card bg-secondary">
+               <div className="card-body secondCard">
+                  The quick, brown fox jumps over a lazy dog. DJs flock by when
+                  MTV ax quiz prog. Junk MTV quiz graced by fox whelps. Bawds
+                  jog, flick quartz, vex nymphs. Waltz, bad nymph, for quick
+                  jigs vex! Fox nymphs grab quick-jived waltz. Brick quiz whangs
+                  jumpy veldt fox.
                </div>
             </div>
          </div>
-      </div>
+         <button href="edit.html" className="btn btn-link">
+            Edit card
+         </button>
+         <div className="float-right">
+            <button
+               href="review-done.html"
+               className="btn btn-outline-primary mr-1"
+            >
+               Needs Work
+            </button>
+            <button href="review-done.html" className="btn btn-primary">
+               <img
+                  className="mb-2"
+                  src={thumbsUpIcon}
+                  width="20px;"
+                  style={{ marginRight: "7px" }}
+                  alt=""
+               />
+               Got it
+            </button>
+         </div>
+      </AppTemplate>
    );
 }
