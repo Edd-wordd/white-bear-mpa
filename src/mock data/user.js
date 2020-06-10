b715dc56a0d28237fe8c9ac5b881e62a69ce1e5f
Edd-wordd <edward.plasencio@gmail.com>
2020-06-10 11:17:28 -0700

    add user data

---
+++ src/mock data/user.js
@@ -0,0 +1,15 @@
+const user = [
+   {
+      id: "8c3ee2c4-d34d-4fe1-a438-e9ea9271c910",
+      email: "edward@gmail.com",
+      password: "68B397755DBBFBBD2CA61395B96601542C3B7C1C",
+      createdOn: 1596243680,
+   },
+
+   {
+      id: "77d9c8ec-f5d6-4d17-bd69-61cf347bfe55",
+      email: "sara@gmail.com",
+      password: "3D3F2FDD6AEBFB096BA89A595DC205E2A164BCF3",
+      createdOn: 941591543,
+   },
+];
